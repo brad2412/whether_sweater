706da4de8f85213df20042045a1f0b87e3aff8fd
Bradley Milton <bmilton2412@gmail.com>
2023-09-25 11:04:54 -0700

    fixes

--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -1,6 +1,6 @@
 class BookService
   def self.search_books(location, quantity)
-    get_url("search.json?q=#{location}&limit=#{quantity}")
+    get_url("api/books?bibkeys=ISBN:#{location}&format=json")
   end
 
   def self.get_url(url)
@@ -9,6 +9,6 @@ class BookService
   end
 
   def self.conn
-    Faraday.new(url: "https://api.openlibrary.org/")
+    Faraday.new(url: "https://openlibrary.org/")
   end
 end
